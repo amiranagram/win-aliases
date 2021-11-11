18d8dbe707028db6bb0bc6b7dbf4d781cee0f778
Amir <a.rami@adaptech.me>
2021-11-11 16:58:50 +0100

    Add queue worker & websockets

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,6 +4,18 @@ function runArtisan($arg1, $arg2, $arg3, $arg4, $arg5) {
         $arg2 = '--seed'
     }
 
+    if ($arg1 -eq 'ws') {
+        $arg1 = 'websockets:serve'
+    }
+
+    if ($arg1 -eq 'qw') {
+        $arg1 = 'queue:work'
+    }
+
+    if ($arg1 -eq 'ql') {
+        $arg1 = 'queue:listen'
+    }
+
     php artisan $arg1, $arg2, $arg3, $arg4, $arg5
 }
 
